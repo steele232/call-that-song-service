fbc46c694200dd564e2a061522e87d9c0988e7e1
Jonathan Steele <jsteele@avercast.com>
2019-03-14 09:12:43 -0600

    Made addSong() to actually insert a (dummy) song, when it receives a request.

--- songs.go
+++ songs.go
@@ -11,7 +11,7 @@ import (
 func createSongsTableIfNotExists() error {
 
 	createTableStr :=
-		`CREATE TABLE IF NOT EXISTS songs ( id INTEGER PRIMARY KEY, name VARCHAR NOT NULL, url VARCHAR NOT NULL,  originalViews INTEGER NOT NULL, latestViews INTEGER NOT NULL );`
+		`CREATE TABLE IF NOT EXISTS songs ( id BIGSERIAL PRIMARY KEY, name VARCHAR NOT NULL, url VARCHAR NOT NULL,  originalViews INTEGER NOT NULL, latestViews INTEGER NOT NULL );`
 
 	if _, err := db.Exec(createTableStr); err != nil {
 		return err
@@ -53,35 +53,26 @@ func getSongs(c *gin.Context) {
 }
 
 func addSong(c *gin.Context) {
+
+	songName := "Jonathan"
+
 	err := createSongsTableIfNotExists()
 	if err != nil {
 		c.String(http.StatusInternalServerError,
 			fmt.Sprintf("Error creating database table: %q", err))
 	}
 
-	if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-		c.String(http.StatusInternalServerError,
-			fmt.Sprintf("Error incrementing tick: %q", err))
-		return
-	}
-
-	rows, err := db.Query("SELECT tick FROM ticks")
+	_, err = db.Exec("INSERT INTO songs(name, url, originalviews, latestviews) VALUES (?, ?, ?, ?);",
+		songName, "www.youtube.com", 0, 1)
 	if err != nil {
 		c.String(http.StatusInternalServerError,
-			fmt.Sprintf("Error reading ticks: %q", err))
+			fmt.Sprintf("Error inserting song: %q", err))
 		return
 	}
-
-	defer rows.Close()
-	for rows.Next() {
-		var tick time.Time
-		if err := rows.Scan(&tick); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error scanning ticks: %q", err))
-			return
-		}
-		c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-	}
+	c.String(
+		http.StatusCreated,
+		fmt.Sprintf("Success inserting song %q", songName)
+	)
 }
 
 func updateSong(c *gin.Context) {
